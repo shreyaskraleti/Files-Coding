75d0010b9245158803a3048b07bf64e049c1c245
shreyaskraleti <shreyas.kraleti2@gmail.com>
2025-02-21 14:02:57 +0530

    python and html files are added

---
+++ pydj-files/Python files/factorial.py
@@ -0,0 +1,10 @@
+# code to find the factorial of a number:
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+    
+print(factorial(3))
+print(factorial(5))
+print(factorial(10))
