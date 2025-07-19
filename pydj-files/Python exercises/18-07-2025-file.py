ad132f2583330157fd4f867af96f7faabac972d7
shreyaskraleti <shreyas.kraleti2@gmail.com>
2025-07-19 10:33:48 +0530

    phani python files added

---
+++ pydj-files/Python exercises/18-07-2025-file.py
@@ -0,0 +1,16 @@
+# sum
+num1 = int(input("enter first number:"))
+num2 = int(input("enter second number:"))
+sum = num1 + num2
+print("sum is:", sum)
+
+# area of a square
+s = float(input("enter side:"))
+area = s * s
+print("area of a square is:", area)
+
+# average
+num1 = float(input("enter first number:"))
+num2 = float(input("enter second number:"))
+average = (num1 + num2)/2
+print("average of 2 numbers is:", average)
---
+++ pydj-files/Python exercises/19-07-2025-Phani-file.py
@@ -0,0 +1,10 @@
+# coding questions - Phani:
+# 1. sum of 2 integers taken as input:
+num1=int(input("Enter number 1 : "))
+num2=int(input("Enter number 2 : "))
+sum=num1+num2
+print("Sum of 2 numbers is:", sum)
+# 2. area of a square is:
+side=int(input("enter side of square: "))
+area=side*side
+print("area of square is : ", area)
